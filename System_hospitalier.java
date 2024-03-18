786ec229733bdf00aa9fecccfb717085618da150
dung <danghoangdung2004@gmail.com>
2024-03-18 09:47:58 +0100

    Ajouter les commentaires dans fiche System_hospitalier.java

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -34,12 +34,12 @@ public class System_hospitalier {
     }
 
     private String analyserRequete(Requête requête) {
-        
+        // Fonction pour annalyser les requetes
         return "requête analysée";
     }
 
     private String comparerDonnes() {
-        
+        // Fonction pour comparer les Donnes
         return "Cluster trouvé";
     }
 
